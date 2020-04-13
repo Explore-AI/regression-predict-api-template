dd30095c4e69f2d27076967f293d72edc9c25ead
JonGerrand <gerrand.jonathan@gmail.com>
2020-04-13 14:27:03 +0200

    Update target URL

--- flask_app/api.py
+++ flask_app/api.py
@@ -30,4 +30,4 @@ def model_prediction():
 
 # Configure Server Startup properties
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- flask_app/assets/request.py
+++ flask_app/assets/request.py
@@ -8,6 +8,6 @@
 
 import requests
 
-url = 'http://127.0.0.1:5000/api'
+url = 'http://3.248.213.125:5000/api'
 r = requests.post(url, json={'exp':2.2,})
 print(r.json())
